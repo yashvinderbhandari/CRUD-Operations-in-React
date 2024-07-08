7bd41d8542451676eccccd1e1785831687c0e48b
yash <yashvinderbhandari@gmail.com>
2024-07-08 13:32:11 +0530

    Doing crud operation in List

---
+++ listproject/src/App.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import './App.css';
+import Home from './components/Home';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
+import Add from './components/Add';
+import Edit from './components/Edit';
+
+function App() {
+  return (
+    <div className="App">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/create" element={<Add/>} />
+          <Route path="/edit" element={<Edit/>} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
---
+++ listproject/src/components/Employees.js
@@ -0,0 +1,23 @@
+const Employees = [
+
+    {
+        id:"1",
+        Name:"Ben",
+        Age:"30"
+
+    },
+    {
+        id:"2",
+        Name:"gwen",
+        Age:"31"
+
+    },
+    {
+        id:"3",
+        Name:"horn",
+        Age:"28"
+
+    }
+]
+
+export default Employees;
